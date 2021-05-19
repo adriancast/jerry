c65591f8c5d79f06d88dc3c78236b1f35e4432f4
adriancast <hello.adriancast@gmail.com>
2021-05-20 00:23:51 +0200

    Initial models, missing validations

--- app/control/admin.py
+++ app/control/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import PortfolioConfiguration, Project
-
+from .models import (
+    PortfolioConfiguration,
+    Project,
+    ProjectWallet,
+    ProjectMilestone
+)
 
 class PortfolioConfigurationAdmin(admin.ModelAdmin):
     list_display = [
@@ -8,15 +12,38 @@ class PortfolioConfigurationAdmin(admin.ModelAdmin):
         'end_date',
         'is_validated',
     ]
-    pass
+
+class ProjectWalletAdmin(admin.ModelAdmin):
+    list_display = [
+        'start_date',
+        'end_date',
+        'description',
+        'is_validated',
+    ]
+
+class ProjectMilestoneAdmin(admin.ModelAdmin):
+    list_display = [
+        'description',
+        'due_date',
+    ]
+
+class ProjectMilestoneInline(admin.TabularInline):
+    model = ProjectMilestone
+
 
 class ProjectAdmin(admin.ModelAdmin):
+    inlines = [
+        ProjectMilestoneInline,
+    ]
     list_display = [
         'name',
         'description',
         'start_date',
+        'priority',
     ]
 
 # Register your models here.
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(PortfolioConfiguration, PortfolioConfigurationAdmin)
+admin.site.register(ProjectMilestone, ProjectMilestoneAdmin)
+admin.site.register(ProjectWallet, ProjectWalletAdmin)
--- app/control/models.py
+++ app/control/models.py
@@ -1,12 +1,55 @@
 from django.db import models
 
 class PortfolioConfiguration(models.Model):
+    name = models.CharField(max_length=256)
     start_date = models.DateField()
     end_date = models.DateField()
     is_validated = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
+class ProjectWallet(models.Model):
+    portfolio_configuration = models.ForeignKey(
+        PortfolioConfiguration,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=256)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    description = models.TextField()
+    is_validated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
 
 class Project(models.Model):
+    wallet = models.ForeignKey(
+        ProjectWallet,
+        on_delete=models.CASCADE,
+    )
+    priority = models.PositiveIntegerField(default=10)
     name = models.CharField(max_length=256)
     description = models.TextField()
     start_date = models.DateField()
+
+    is_cancelled = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+
+class ProjectMilestone(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    due_date = models.DateField()
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
