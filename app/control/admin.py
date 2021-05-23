cd905750022ffd008415589f2d9eb471b0dc801b
adriancast <hello.adriancast@gmail.com>
2021-05-23 19:17:03 +0200

    Models in progress

--- app/control/admin.py
+++ app/control/admin.py
@@ -3,32 +3,118 @@ from .models import (
     PortfolioConfiguration,
     Project,
     ProjectWallet,
-    ProjectMilestone
+    ProjectMilestone,
+    PortfolioConfigurationGeneralManagerRevision
 )
+
+class ProjectWalletInline(admin.TabularInline):
+    model = ProjectWallet
+    extra = 0
+    can_delete = False
+    show_change_link = True
+    readonly_fields = [
+        'name',
+        'start_date',
+        'end_date',
+        'description',
+    ]
+    def has_add_permission(self, request, obj=None):
+        return False
+
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class PortfolioConfigurationGeneralManagerRevisionInline(admin.TabularInline):
+    model = PortfolioConfigurationGeneralManagerRevision
+    extra = 0
+    show_change_link = True
+    can_delete = False
+    readonly_fields = [
+        'comment',
+        'is_validated'
+    ]
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+class PortfolioConfigurationGeneralManagerRevisionAdmin(admin.ModelAdmin):
+    list_display = [
+        'portfolio_configuration',
+        'comment',
+        'is_validated',
+    ]
 
 class PortfolioConfigurationAdmin(admin.ModelAdmin):
     list_display = [
+        'name',
         'start_date',
         'end_date',
-        'is_validated',
+        'total_budget_eur'
     ]
+
+    inlines = [
+        PortfolioConfigurationGeneralManagerRevisionInline,
+        ProjectWalletInline,
+    ]
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    can_delete = False
+    extra = 0
+    show_change_link = True
+    readonly_fields = [
+        'priority',
+        'name',
+        'description',
+        'start_date',
+        'is_cancelled',
+        'required_hours'
+    ]
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 class ProjectWalletAdmin(admin.ModelAdmin):
     list_display = [
         'start_date',
         'end_date',
         'description',
-        'is_validated',
     ]
+    inlines = [
+        ProjectInline,
+    ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['portfolio_configuration'].queryset = PortfolioConfiguration.objects.filter(
+            general_manager_revision__is_validated=True
+        )
+        return form
+
 
 class ProjectMilestoneAdmin(admin.ModelAdmin):
     list_display = [
-        'description',
+        'name',
         'due_date',
+        'status',
+    ]
+    search_fields = [
+        'name',
+    ]
+    list_filter = [
+        'status'
     ]
 
 class ProjectMilestoneInline(admin.TabularInline):
     model = ProjectMilestone
+    show_change_link = True
+    extra = 0
+    can_delete = False
 
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -40,10 +126,39 @@ class ProjectAdmin(admin.ModelAdmin):
         'description',
         'start_date',
         'priority',
+        'category',
+        'required_hours',
+        'delta_roi',
+        'delayed_tasks',
+        'delayed_tasks_percentage',
+        'is_cancelled',
     ]
+
+    readonly_fields = [
+        'delta_roi',
+        'completed_tasks',
+        'is_cancelled',
+    ]
+
+    list_filter = [
+        'category',
+        'priority',
+        'is_cancelled',
+    ]
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        form.base_fields['wallet'].queryset = ProjectWallet.objects.filter(
+            is_open=True
+        )
+        return form
+
+    def get_readonly_fields(self, request, obj=None):
+        return self.readonly_fields
 
 # Register your models here.
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(PortfolioConfiguration, PortfolioConfigurationAdmin)
 admin.site.register(ProjectMilestone, ProjectMilestoneAdmin)
 admin.site.register(ProjectWallet, ProjectWalletAdmin)
+admin.site.register(PortfolioConfigurationGeneralManagerRevision, PortfolioConfigurationGeneralManagerRevisionAdmin)
--- app/control/models.py
+++ app/control/models.py
@@ -1,24 +1,62 @@
 from django.db import models
+from datetime import datetime, date
 
 class PortfolioConfiguration(models.Model):
     name = models.CharField(max_length=256)
     start_date = models.DateField()
     end_date = models.DateField()
-    is_validated = models.BooleanField(default=False)
+
+    total_budget_eur = models.PositiveIntegerField()
+    dev_resources_hours = models.PositiveIntegerField()
+    sysops_resources_hours = models.PositiveIntegerField()
+    management_resources_hours = models.PositiveIntegerField()
+    marketing_resources_hours = models.PositiveIntegerField()
+    operative_resources_hours = models.PositiveIntegerField()
+
+    @property
+    def is_validated_by_manager(self):
+        default = False
+        has_general_manager_revision = bool(self.general_manager_revision)
+        return (
+            self.general_manager_revision.is_validated
+            if has_general_manager_revision
+            else default
+        )
 
     def __str__(self):
         return self.name
 
-class ProjectWallet(models.Model):
+class PortfolioConfigurationGeneralManagerRevision(models.Model):
     portfolio_configuration = models.ForeignKey(
         PortfolioConfiguration,
         on_delete=models.CASCADE,
+        related_name='general_manager_revision'
+    )
+
+    is_validated = models.BooleanField(default=False)
+
+    # total_budget_eur = models.PositiveIntegerField(blank=True, null=True)
+    # dev_resources_hours = models.PositiveIntegerField(blank=True, null=True)
+    # sysops_resources_hours = models.PositiveIntegerField(blank=True, null=True)
+    # management_resources_hours = models.PositiveIntegerField(blank=True, null=True)
+    # marketing_resources_hours = models.PositiveIntegerField(blank=True, null=True)
+    # operative_resources_hours = models.PositiveIntegerField(blank=True, null=True)
+
+    comment = models.TextField(blank=True, null=True)
+
+
+class ProjectWallet(models.Model):
+    portfolio_configuration = models.OneToOneField(
+        PortfolioConfiguration,
+        on_delete=models.CASCADE,
+        primary_key=True,
     )
     name = models.CharField(max_length=256)
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.TextField()
-    is_validated = models.BooleanField(default=False)
+
+    is_open = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
@@ -26,6 +64,25 @@ class ProjectWallet(models.Model):
 
 
 class Project(models.Model):
+    STATUS_PENDING = 'PENDING'
+    STATUS_IN_PROGRESS = 'IN_PROGRESS'
+    STATUS_BLOCKED = 'BLOCKED'
+    STATUS_DONE = 'DONE'
+    STATUS_CANCELLED = 'CANCELLED'
+    STATUS_CHOICES = [
+        (STATUS_PENDING, 'Pending'),
+        (STATUS_IN_PROGRESS, 'In progress'),
+        (STATUS_BLOCKED, 'Blocked'),
+        (STATUS_DONE, 'Done'),
+        (STATUS_CANCELLED, 'Cancelled'),
+    ]
+
+    status = models.CharField(
+        max_length=32,
+        choices=STATUS_CHOICES,
+        default=STATUS_PENDING,
+    )
+
     wallet = models.ForeignKey(
         ProjectWallet,
         on_delete=models.CASCADE,
@@ -35,13 +92,58 @@ class Project(models.Model):
     description = models.TextField()
     start_date = models.DateField()
 
+    required_hours = models.PositiveIntegerField()
+
+    category = models.CharField(max_length=256, blank=True)
+
+    estimated_roi = models.PositiveIntegerField()
+    real_roi = models.IntegerField()
+
     is_cancelled = models.BooleanField(default=False)
+
+    # Internal fields
+    delta_roi = models.FloatField(blank=True, null=True)
+    completed_tasks = models.FloatField(blank=True, null=True)
+    delayed_tasks = models.IntegerField(blank=True, null=True)
+    delayed_tasks_percentage = models.FloatField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.delta_roi = round(self.real_roi / self.estimated_roi, 2)
+        completed_milestones = len(self.milestones.filter(status=ProjectMilestone.STATUS_DONE))
+        total_milestones = len(self.milestones.all())
+        self.completed_tasks = completed_milestones / total_milestones
+        self.delayed_tasks = len(
+            self.milestones.filter(due_date__lt=date.today())
+        )
+        self.delayed_tasks_percentage = self.delayed_tasks / total_milestones
+
+        self.is_cancelled = False
+        if self.status == self.STATUS_CANCELLED:
+            self.is_cancelled = True
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
 
 
 class ProjectMilestone(models.Model):
+    STATUS_PENDING = 'PENDING'
+    STATUS_IN_PROGRESS = 'IN_PROGRESS'
+    STATUS_BLOCKED = 'BLOCKED'
+    STATUS_DONE = 'DONE'
+    STATUS_CHOICES = [
+        (STATUS_PENDING, 'Pending'),
+        (STATUS_IN_PROGRESS, 'In progress'),
+        (STATUS_BLOCKED, 'Blocked'),
+        (STATUS_DONE, 'Done'),
+    ]
+
+    status = models.CharField(
+        max_length=32,
+        choices=STATUS_CHOICES,
+        default=STATUS_PENDING,
+    )
     name = models.CharField(max_length=256)
     description = models.TextField()
     due_date = models.DateField()
@@ -49,7 +151,12 @@ class ProjectMilestone(models.Model):
     project = models.ForeignKey(
         Project,
         on_delete=models.CASCADE,
+        related_name='milestones',
     )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.project.save()
 
     def __str__(self):
         return self.name
