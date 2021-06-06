51ba09253cbb3c2ef1ccfaa31409986d5406d070
adriancast <hello.adriancast@gmail.com>
2021-06-06 18:33:24 +0200

    Fix media urls

--- app/jerry/urls.py
+++ app/jerry/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     path('', admin.site.urls),
 ]
